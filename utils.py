a82c67ab82c19127f3cdc84418b6d7ecf6398966
Felix Labelle <flx.labelle@gmail.com>
2023-03-22 19:47:01 -0400

    Added simple progress bar

--- utils.py
+++ utils.py
@@ -14,3 +14,29 @@ class PickleBaseClass:
             
 def mean(lst):
     return sum(lst)/len(lst)
+
+# https://www.youtube.com/watch?v=31Wjc9vZv1s
+class ProgressBar:
+    def __init__(self, iterable, max_steps=0):
+        if max_steps <= 0:
+            self.max_steps = len(iterable)
+        else:
+            self.max_steps = max_steps
+        self.current_iter = 0
+        self.wrapped_iter = iter(iterable)
+        self.print_size = 20
+        
+    def _print(self):
+        percentage_complete = self.current_iter / self.max_steps
+        num_symbols = round(self.print_size * percentage_complete)
+        
+        print("[" + "#" * num_symbols + "=" * (self.print_size-num_symbols) + "]" + f" {percentage_complete * 100:.2f}%",end="\r",sep="")
+        
+    def __iter__(self):
+        while self.current_iter < self.max_steps:
+            self._print()
+            yield next(self.wrapped_iter)
+            self.current_iter += 1
+        print()
+    def __len__(self):
+        return self.max_steps
